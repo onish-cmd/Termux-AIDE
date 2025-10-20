5a0607c5fa93e7e018ae142abe981e3e0a5cf78a
onish-cmd <136346254+onish-cmd@users.noreply.github.com>
2025-10-20 14:27:33 +0530

    Update build.gradle.kts

--- MobileIDE/build.gradle.kts
+++ MobileIDE/build.gradle.kts
@@ -1,5 +1,5 @@
 // build.gradle.kts (root)
 plugins {
-  id("com.android.application") version "8.7.0" apply false
+  id("com.android.application") version "8.5.2" apply false
   kotlin("android") version "2.0.0" apply false
 }
