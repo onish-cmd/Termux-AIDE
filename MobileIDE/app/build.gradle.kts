497e222bf5e2859ccd4498b5f09d7ff3b9576d42
onish-cmd <136346254+onish-cmd@users.noreply.github.com>
2025-10-20 14:24:20 +0530

    Update build.gradle.kts

--- MobileIDE/app/build.gradle.kts
+++ MobileIDE/app/build.gradle.kts
@@ -1,5 +1,6 @@
 // app/build.gradle.kts
 plugins {
+    id("com.android.application") version "8.5.2"
     id("org.jetbrains.kotlin.android") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
